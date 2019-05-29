5a26443586c1d26a42a66461bcf771280dcfe925
aunee1234 <49961576+aunee1234@users.noreply.github.com>
2019-05-29 17:52:04 -0400

    i am adding pom.xml and src

---
+++ src/main/java/com/main/Amazon.java
@@ -0,0 +1,46 @@
+package com.main;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Amazon {
+	  public static void main(String[] args) throws Throwable {
+	WebDriver driver;
+	  System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+	  driver=new ChromeDriver(); driver.get("https://www.amazon.com");
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+	  Select slt = new Select (driver.findElement(By.xpath("//select[@class='nav-search-dropdown searchSelect']")));
+		slt.selectByVisibleText("Baby");
+		WebElement we = slt.getFirstSelectedOption();
+		System.out.println(we);
+		System.out.println("=====================");
+		List<WebElement> option = slt.getOptions();
+		System.out.println(option.size());
+		System.out.println("=====================");
+		for(int i=0; i<option.size();i++) {
+			
+		System.out.println(option.get(i).getText());
+		//System.out.println("======================");
+		//Select sl = new Select (driver.findElement(By.xpath("//select[@class='nav-search-dropdown searchSelect']")));
+	   // sl.selectByVisibleText("Baby");
+	    //List<WebElement> gtlst = sl.getOptions();
+	 
+		if(option.get(i).getText().equals(slt.getFirstSelectedOption().getText())){
+			
+            System.out.println("The index of the selected option is: "+ i);
+            System.out.println("=====================");
+           // break;
+            }
+                    
+            }
+            
+            
+}
+	  }
---
+++ src/test/java/com/tt/testngrunner/LoginRunner.java
@@ -0,0 +1,18 @@
+package com.tt.testngrunner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		plugin= {"pretty","html:target","com.cucumber.listener.ExtentCucumberFormatter:target/Cucumber_Extents_report.html"},
+		features= "./Feature/",
+		glue="com.tt.stepdef"
+		//plugin= {"pretty","html:target","com.cucumber.listener.ExtentCucumberFormatter:target/Cucumber_Extents_report.html"},
+		//features= {"Feature"},
+		//glue= {"com.tt.stepdef"},
+		//tags= {"@loging","@dropdown"}
+		
+		)
+public class LoginRunner extends AbstractTestNGCucumberTests{
+
+}
